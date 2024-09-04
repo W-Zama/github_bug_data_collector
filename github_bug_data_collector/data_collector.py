bbd0fd79d9358a04813f2cc43508876200f1f42a
W-Zama <wzama4667@gmail.com>
2024-09-05 03:13:16 +0900

    パッケージ配布用に更新

--- github_bug_data_collector/data_collector.py
+++ github_bug_data_collector/data_collector.py
@@ -41,16 +41,16 @@ class DataCollector:
     }
 
     @classmethod
-    def get_column_map_of_issues(cls):
+    def get_column_map_of_issues(cls) -> dict[str, str]:
         """column_map_of_issuesのゲッタ"""
         return cls.COLUMN_MAP_OF_ISSUES
 
     @classmethod
-    def get_column_map_of_users(cls):
+    def get_column_map_of_users(cls) -> dict[str, str]:
         """column_map_of_usersのゲッタ"""
         return cls.COLUMN_MAP_OF_USERS
 
-    def __init__(self, access_token: str):
+    def __init__(self, access_token: str) -> None:
         """
         DataCollectorクラスの初期化メソッド。
 
@@ -60,20 +60,28 @@ class DataCollector:
 
         self.github = Github(auth=github.Auth.Token(access_token))
 
-    def show_rate_limit(self):
+    def show_rate_limit(self) -> None:
         """リクエスト制限に関する情報を表示する"""
         print(
-            f"Remaining request count: {f"{self.github.rate_limiting[0]} / {self.github.rate_limiting[1]}"}")
+            f"Remaining request count: {f'{self.github.rate_limiting[0]} / {self.github.rate_limiting[1]}'}")
         print(
             f"Request reset time: {datetime.fromtimestamp(self.github.rate_limiting_resettime)}")
 
-    def generate_csv(self, owner: str, repo_name: str, dir_path: str = ".", state: str = "all", labels: str | None = None, limit: int | None = None):
+    def check_limit_and_wait(self) -> None:
+        """リクエスト制限を確認し，リクエストが可能でないなら可能になるまで待機する"""
+        if self.github.rate_limiting[0] <= 0:
+            print("Request limit exceeded. Waiting for reset time...")
+            while datetime.now().timestamp() < self.github.rate_limiting_resettime:
+                pass
+
+    def generate_csv(self, owner: str, repo_name: str, dir_path: str = ".", state: str = "all", labels: list[str] | None = None, limit: int | None = None) -> None:
         """issuesに基づいたCSVを生成する"""
 
         repo = self.github.get_repo(f"{owner}/{repo_name}")
 
         # labelsがNoneの場合にはデフォルト値を使用
         issues = repo.get_issues(state=state, labels=labels or [])
+        self.check_limit_and_wait()
 
         total_issues = issues.totalCount
 
@@ -98,6 +106,8 @@ class DataCollector:
                     break
                 print(f"Processing issue {i+1}/{total_issues}")
 
+                self.check_limit_and_wait()
+
                 row = []    # CSVの各要素を格納するリスト
 
                 # 前のIssueとの時間差を取得
@@ -105,7 +115,8 @@ class DataCollector:
                     time_to_next_issue = None
                 else:
                     current_created_at = issue.created_at
-                    time_to_next_issue = (previous_created_at - current_created_at).total_seconds()
+                    time_to_next_issue = (
+                        previous_created_at - current_created_at).total_seconds()
 
                 row.append(str(time_to_next_issue))
 
@@ -125,6 +136,7 @@ class DataCollector:
 
                 # user.loginの情報を取得
                 user_info = self.github.get_user(issue.user.login).__dict__
+                self.check_limit_and_wait()
 
                 # 各カラムの値を取得
                 for column, json_path in column_map_of_users.items():
